57d5a266e8f0b18c82400a339ee10d9a739afc79
rovenelabanga <rovenelabanga2001@gmail.com>
2024-09-30 16:48:59 +0300

    uses array method find

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+  const superbowlWin  = (array) => {
+    const win = array.find(array => array.result === "W")
+    return win ? win.year : undefined;
+  }
+
+  console.log(superbowlWin(record))
